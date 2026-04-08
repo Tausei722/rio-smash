eb6c34e7d1a9f8dc414d63dbd245536b0ef10dff
Tausei722 <kusaki.kei825@gmail.com>
2026-04-08 23:18:10 +0900

    DB移行

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const SUPABASE_URL = 'https://ehqhipghekqkrhfromef.supabase.co';
+const SUPABASE_URL = 'https://xilrpvnierjjuouduqsg.supabase.co';
 const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVocWhpcGdoZWtxa3JoZnJvbWVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3Njg2MjgsImV4cCI6MjA5MDM0NDYyOH0.ZjLuW55Uazd2AlUIvHC59oNX8kBotgwGLsncVm9DBhc';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpbHJwdm5pZXJqanVvdWR1cXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NDkwMTYsImV4cCI6MjA5MTIyNTAxNn0.PYzS3cvJ6ZRIxgVYzaBYa9emsNg8aw4hlhs5gyeFOoE';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
