5be3c3b24cfe41cc55de4cb3a884b01b6e668d20
Tausei722 <kusaki.kei825@gmail.com>
2026-05-02 01:18:41 +0900

    ci: install node via brew before pod install

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 set -e
 
-# Install Node dependencies
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+# Add Homebrew to PATH
+if [[ $(uname -m) == 'arm64' ]]; then
+  export PATH="/opt/homebrew/bin:$PATH"
+else
+  export PATH="/usr/local/bin:$PATH"
+fi
+
+# Install Node.js
+brew install node
+
+# Install npm dependencies
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 npm install
 
-# Run pod install (CocoaPods is pre-installed on Xcode Cloud)
+# Run pod install
 cd "$CI_PRIMARY_REPOSITORY_PATH/ios"
 pod install
