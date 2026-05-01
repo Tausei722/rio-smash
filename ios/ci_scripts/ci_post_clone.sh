a0d71f70f6146f806a78b7d224acbd55a5e1f301
Tausei722 <kusaki.kei825@gmail.com>
2026-05-02 01:12:51 +0900

    ci: simplify post-clone script, use pre-installed pod

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 set -e
 
-# Install Homebrew if missing
-if ! command -v brew &> /dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Install CocoaPods
-brew install cocoapods
-
-# Install Node & npm dependencies
+# Install Node dependencies
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 npm install
 
-# Run pod install
-cd ios
+# Run pod install (CocoaPods is pre-installed on Xcode Cloud)
+cd "$CI_PRIMARY_REPOSITORY_PATH/ios"
 pod install
